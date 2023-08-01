d989e9929222e0112f7364e82d73ca5636d5a1be
Patryk Kurek <patryk.kurekfb@gmail.com>
2023-08-01 17:58:49 +0200

    second commit

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 const email = document.querySelector('#email');
 const submit = document.querySelector('button');
 const errorM = document.querySelector('#errorM');
-const mainCard = document.querySelector('#card');
+const mainCard = document.querySelector('.inCardContent');
+const Card = document.querySelector('#card');
+const successMessage = document.querySelector('.successMessage');
 
 
 email.addEventListener('change', function() {
@@ -14,7 +16,12 @@ email.addEventListener('change', function() {
 
         submit.addEventListener('click', function() {
             mainCard.classList.add('hide');
+            mainCard.classList.remove('inCardContent');
+            successMessage.classList.remove('hide');
+
+
             console.log("test");
+            console.log(mainCard.classList);
         })
         
     }else{
